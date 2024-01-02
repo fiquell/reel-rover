e8f197a957130e4b62fb9cc6293020491d4d6205
Fiquell <fiquellh@gmail.com>
2024-01-03 00:02:19 +0800

    This branch is so dirty, even your mom can't clean it.

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSameLine: true,
+  plugins: ['prettier-plugin-tailwindcss']
+}
